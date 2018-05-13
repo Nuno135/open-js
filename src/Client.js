3be40e2496c9c9cc954433b2132d0705203de138
Nuno135 <nuno.n@post.com>
2018-05-13 16:21:06 +0200

    Update Client.js

--- src/Client.js
+++ src/Client.js
@@ -5,11 +5,11 @@ const EventEmitter = require('events').EventEmitter;
 class Client extends EventEmitter {
     constructor(token, options) {
         super();
-
-        this.options = {}
-
-        this.token = token;
-
         
+        this.options = options;
+
+        this.token = token; 
     }
-}
+};
+
+module.exports = Client;
