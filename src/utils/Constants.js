c49e105a3b2f12f699b1a01a9592dbf1843a6422
Nuno135 <nuno.n@post.com>
2018-05-13 20:59:25 +0200

    Fixed an issue and added events

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -27,35 +27,6 @@ exports.VoiceOPCodes = {
     CLIENT_DISCONNECTED: 13
 };
 
-exports.OPCodes = {
-    DISPATCH: 0,
-    HEARTBEAT: 1,
-    IDENTIFY: 2,
-    STATUS_UPDATE: 3,
-    VOICE_STATE_UPDATE: 4,
-    VOICE_SERVER_PING: 5,
-    RESUME: 6,
-    RECONNECT: 7,
-    REQUEST_GUILD_MEMBERS: 8,
-    INVALID_SESSION: 9,
-    HELLO: 10,
-    HEARTBEAT_ACK: 11
-};
-
-exports.VoiceOPCodes = {
-    IDENTIFY: 0,
-    SELECT_PROTOCOL: 1,
-    READY: 2,
-    HEARTBEAT: 3,
-    SESSION_DESCRIPTION: 4,
-    SPEAKING: 5,
-    HEARTBEAT_ACK: 6,
-    RESUME: 7,
-    HELLO: 8,
-    RESUMED: 9,
-    CLIENT_DISCONNECTED: 13
-};
-
 exports.Events = {
     READY: 'ready',
     RESUMED: 'resumed',
